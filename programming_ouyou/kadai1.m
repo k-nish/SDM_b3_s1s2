0b1fcd5c43580a3a78a950f8eecb06a6ae6247d2
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-04-14 14:30:53 +0900

    finished assignment kadai1

--- programming_ouyou/kadai1.m
+++ programming_ouyou/kadai1.m
@@ -3,8 +3,17 @@ M=3;
 L=3;
 dx = 0.1 / ( M + 1);
 dy = 0.1 / ( L + 1);
+
+%x,y両方向の差分逆数二乗和の二倍をa_1とし、またx,y方向の差分逆数二乗をそれぞれa_2,a_3とする
+a_1 = -2 / dx^2 - 2 / dy^2;
+a_2 = 1 / dx^2;
+a_3 = 1 / dy^2;
+
+
 %pを単位行列として作成
 p = eye(M * L);
+
+
 %対角項にa_1を代入
 for i = 1: size(p)
 	p(i,i) = a_1;
@@ -22,76 +31,95 @@ f(1,:) = 100;
 %psaiをML*1のゼロ行列として作成
 psai = zeros(M * L,1);
 
-%x,y両方向の差分逆数二乗和の二倍をa_1とし、またx,y方向の差分逆数二乗をそれぞれa_2,a_3とする
-a_1 = -2 / dx^2 -2 / dy^2;
-a_2 = 1 / dx^2;
-a_3 = 1 / dy^2;
+for i=1:L
+	for j=1:M
+		%aを定義
+		a = (j - 1) * L + (i - 1) + 1;
+		%a = 1のとき
+		if(i == 1 && j ==1)
+			%3.の処理
+			p(a,a + 1) = a_2;
+			%5.の処理
+			p(a,a + L) = a_3;
+		%a=4のとき
+		else if(i == 1 && 1 < j && j < M)
+			%3.の処理
+			p(a,a + 1) = a_2;
+			%4.5.の処理
+			p(a,a - L) = a_2;
+			p(a,a + L) = a_3;
+		%a=7のとき
+		else if(i == 1 && j == M)
+			%3.の処理
+			p(a,a + 1) = a_2;
+			%4.の処理
+			p(a,a - L) = a_3;
+			%5.の処理
+			psai(a,1) = -a_3*100;
+		%a=2のとき
+		else if( 1 < i && i < L && j == 1)
+			%2,3の処理
+			p(a,a-1) = a_2;
+			p(a,a+1) = a_3;
+			%5.の処理
+			p(a,a + L) = a_3;
+		%a=5のとき
+		else if( 1 < i && i < L && 1 < j && j < M)
+			%2,3の処理
+			p(a,a-1) = a_2;
+			p(a,a+1) = a_3;
+			%4.5の処理
+			p(a,a - L) = a_2;
+			p(a,a + L) = a_3;
+		%a=8のとき
+		else if( 1 < i && i < L && j == M)
+			%2,3の処理
+			p(a,a-1) = a_2;
+			p(a,a+1) = a_3;
+			%4.の処理
+			p(a,a - L) = a_3;
+			%5.の処理
+			psai(a,1) = -a_3 * 100;
+		%a=3のとき
+		else if( i == L && j == 1)
+			%2.の処理
+			p(a,a - 1) = a_2;
+			%3.の処理
+			psai(a,1) = psai(a,1) .- a_2 * 100;
+			%5.の処理
+			p(a,a + L) = a_3;
+		%a=6のとき
+		else if( i == L && 1 < j && j < M)
+			%2.の処理
+			p(a,a - 1) = a_2;
+			%3.の処理
+			psai(a,1) = psai(a,1) .- a_2 * 100;
+			%4.5の処理
+			p(a,a - L) = a_2;
+			p(a,a + L) = a_3;
+		%a=9のとき
+		else
+			%2.の処理
+			p(a,a - 1) = a_2;
+			%3.の処理
+			psai(a,1) = psai(a,1) .- a_2 * 100;
+			%4.の処理
+			p(a,a - L) = a_3;
+			%5.の処理
+			psai(a,1) = psai(a,1) .- a_3 * 100;
+		endif
+		endif
+		endif
+		endif
+		endif
+		endif
+		endif
+		endif
+	endfor
+endfor
 
-for i = 1:L
-	if (i == 1)
-		for j = 1:M
-			if (j == 1)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			psai(a,1) = psai(a,1) .- a_2 * 100;
-			else if (1 < j < M)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			p(a,a - L) = a_2;
-			p(a,a + L) = a_3;
-			else (j == M)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			psai(a,1) = psai(a,1) .- a_3 *100;
-			endif
-			end
-		end
-	else if (1 < i < L)
-		for j = 1:M
-			if (j == 1)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			p(a,a - 1) = a_2;
-			p(a,a + 1) = a_3;
-			psai(a,1) = psai(a,1) .- a_2 * 100;
-			else if (1 < j < M)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			p(a,a - 1) = a_2;
-			p(a,a + 1) = a_3;
-			p(a,a - L) = a_2;
-			p(a,a + L) = a_3;
-			else (j == M)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			p(a,a - 1) = a_2;
-			p(a,a + 1) = a_3;
-			psai(a,1) = psai(a,1) .- a_3 *100;
-			endif
-			end
-		end
-	else (i == L)
-		for j = 1:M
-			if (j == 1)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			psai(a,1) = psai(a,1) .- a_2 * 100;
-			else if (1 < j < M)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			p(a,a - L) = a_2;
-			p(a,a + L) = a_3;
-			else (j == M)
-			%aを定義
-			a = (j - 1) * L + (i - 1) + 1;
-			psai(a,1) = psai(a,1) .- a_3 *100;
-			endif
-			end
-		end
-	endif
-	end
-end
 
 disp(p);
 disp(f);
-disp(psai');
+disp(psai);
+
