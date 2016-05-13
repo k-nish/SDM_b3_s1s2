ac13bdef22709cec25ab1427ce05a50729858999
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-05-13 19:12:35 +0900

    push changes of pr1.m this file is used for check code of octave

--- programming_ouyou/pr1.m
+++ programming_ouyou/pr1.m
@@ -1,4 +1,14 @@
-A = [1,2,3;4,5,6;7,8,9];
-disp(A);
+%A = [1,2,3;4,5,6;7,8,9];
+%disp(A)
+%A(2,3)
 
-A(2,3)
+%mu = 2 * 10^(-8);
+%disp(mu);
+%mu = 4 * pi * 10^(-7);
+%disp(mu);
+%r_alpha = [i * dx ; j * dy ; 0];
+%disp(r_alpha);
+
+r_alpha = [1;2;3];
+r_beta = [2;4;6];
+r = sum((r_alpha .- r_beta).^2)^(3/2)
