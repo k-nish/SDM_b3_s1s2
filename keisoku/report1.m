56405454b6e96a0e36f2feb57bec91dddb15d2c6
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-05-18 11:00:20 +0900

    push files about assignment in "計測工学"

---
+++ keisoku/report1.m
@@ -0,0 +1,12 @@
+Data = xlsread('11.xls','加入者','B13:E33');
+
+disp(Data)
+
+%2005年を過去、
+%end=360,t=240,
+
+for i=150:240
+	a_i = Data(3:1,)
+endfor
+
+
---
+++ keisoku/test1.m
@@ -0,0 +1,4 @@
+Data = xlsread('data.xls','加入者','B13:E');
+Data_t = transpose(Data);
+
+for i =
