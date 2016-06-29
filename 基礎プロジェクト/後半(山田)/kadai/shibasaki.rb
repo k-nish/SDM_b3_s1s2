98cb9ea4b8e5774bf32c9df6c974684c2152bd60
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-06-29 19:46:18 +0900

    delete files about "基礎プロ後半" cause finished class

--- 基礎プロジェクト/後半(山田)/kadai/shibasaki.rb
+++
@@ -1,6 +0,0 @@
-def shibasaki(i,j,color,field)
-	if field[i][j] == 0 then
-		field[i][j]=color
-	end
-	return field
-end
--- 基礎プロジェクト/後半(山田)/nishimura.rb
+++
@@ -1,9 +0,0 @@
-# $kawakami = ["1","%","2","3","%","4","5","%","9","A"]
-require './kawakami.rb'
-$nishimura = []
-for var in $kawakami do
-	if var != "%" then
-		$nishimura.push(var)
-	end
-end
-print ($nishimura)
